56d68b2366285356aaf8ea6a462accbc7d07a0dc
Bryan Rodriguez <Serphp@hotmail.com>
2023-05-16 11:11:28 -0500

    [+ Dependecies] firebase & angular/fire

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7WbedVpMk1dBnjpttkPn1vi-cid1Z9rg",
+    authDomain: "serph-f9c7e.firebaseapp.com",
+    databaseURL: "https://serph-f9c7e-default-rtdb.firebaseio.com",
+    projectId: "serph-f9c7e",
+    storageBucket: "serph-f9c7e.appspot.com",
+    messagingSenderId: "937690296061",
+    appId: "1:937690296061:web:84f9a2adda111ed8481894",
+    measurementId: "G-TMSG2HY8GE"
+  };
+
+export const environment = {
+  production: false,
+  firebaseConfig
+};
